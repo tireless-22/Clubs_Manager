fa145989a8cf8e2b27ae52d06874cc7a87214a2c
karth <karthiknaveen22022002@gmail.com>
2022-11-15 16:42:21 +0530

    BACKUP

---
+++ pages/api/user/crateMember.js
@@ -0,0 +1,74 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+export default async function handler(
+	req,
+	res
+) {
+	console.log("hello there ************************************************************")
+	console.log(req.body)
+
+	// club, mailId
+
+
+
+	
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				mailId: req.body.mailId
+			}
+		})
+
+		if (!user) {
+			const user = await prisma.user.create({
+				data: {
+					mailId: req.body.mailId,
+					password: req.body.mailId
+
+				},
+			})
+		}
+
+
+		const userclub = await prisma.userClub.create({
+			data: {
+				clubId: req.body.club,
+				userId: req.body.mailId,
+				role: 1
+
+
+			},
+		})
+
+		const userAllClub = await prisma.userClub.create({
+			data: {
+				clubId: "All Clubs",
+				userId: req.body.mailId,
+				role: 1
+
+
+			},
+		})
+
+
+
+
+	}
+	catch (e) {
+		console.log(e)
+	}
+
+
+
+
+
+
+	res.status(200).json("added")
+
+
+}
+
+
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,8 @@ import Link from 'next/link';
 import SideNav from '../components/sideNav';
 import Loading from '../components/loading';
 
+
+
 const Index = () => {
 
   const { data: clubData, error: clubDataError } = useSWRImmutable('/api/club/getAll', getFetcher)
@@ -16,34 +18,58 @@ const Index = () => {
 
 
   if (clubDataError) return <div>failed to load</div>
-  if (!clubData) return <div><Loading/></div>
+  if (!clubData) return <div><Loading /></div>
+
 
   return (
 
     <div className='index_main'>
       <TopNav />
-      <div className="manage_main_div">
+      <div className="landing_main_div">
 
-        <div className="manage_main_div_left">
-          <SideNav />
-          
+        <div className='landing_main_div_college bg-slate-300'>
 
-        
+          <h1>
+            Some crucial information about the College
+          </h1>
+
+
+
+
+
+        </div>
+
+        <div className='landing_main_div_clubs'>
+          {
+            clubData.map((club) => (
+
+
+              <div className='landing_page_club_card'>
+                <div className='landing_page_club_card_image'>
+                  <Image src={`https://firebasestorage.googleapis.com/v0/b/contest-4f331.appspot.com/o/images%2F${club.fileUrl}?alt=media`} width={200} height={200} />
+
+                </div>
+                <div className='landing_page_club_card_image'>
+                  <h1>
+                    {club.name}
+                  </h1>
+                  
+                </div>
+                <div className='landing_page_club_card_image'>
+                  <h1>
+                    {club.description}
+                  </h1>
+                  
+                </div>
+
+              </div>
+
+
+            ))
+          }
+
+
         </div>
-
-
-        <div className='manage_main_div_right'>
-
-          
-         
-
-          
-
-        </div>
-
-
-
-
       </div>
 
     </div>
